a2f8b82990231b5c15315142ede6d0637ed0d1c9
Pramuditha Rajadasa <108863613+PramudiR@users.noreply.github.com>
2024-03-13 15:06:50 +0530

    Add central log manager

---
+++ centralLog.py
@@ -0,0 +1,16 @@
+import logging
+from logging import StreamHandler
+
+def config_log():
+    ## Configure the logging module
+    logging.basicConfig(filename='server.log', level=logging.INFO, 
+                        format='%(asctime)s - %(levelname)s - %(message)s')
+    log_format = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+
+    # Console (stream) handler
+    console_handler = StreamHandler()
+    console_handler.setFormatter(log_format)
+    console_handler.setLevel(logging.INFO)
+
+    # Add both handlers to the root logger
+    logging.getLogger().addHandler(console_handler)
