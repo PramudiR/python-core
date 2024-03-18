1bdeec9945d6b448e084eb9e3427666d0a05a18a
Pramuditha Rajadasa <108863613+PramudiR@users.noreply.github.com>
2024-03-19 00:47:33 +0530

    get dataset size

---
+++ data_science/prep.py
@@ -0,0 +1,30 @@
+'''Functions for data loading, cleaning and preparing '''
+import logging
+import pandas as pd
+
+
+def get_shape_csv(file_path: str) -> tuple[int, int] | tuple[None, None]:
+    '''Get the size of a CSV data file without loading'''
+    try:
+        df = pd.read_csv(file_path, nrows=0)
+        return df.shape
+    except FileNotFoundError as e:
+        logging.info("File not found: %e", e)
+    except pd.errors.ParserError as e:
+        logging.info("Parsing error: %e", e)
+    except pd.errors.EmptyDataError as e:
+        logging.info("Empty file: %e", e)
+    except UnicodeDecodeError as e:
+        logging.info("Decode error: %e", e)
+    except Exception as e:
+        logging.info("Error: %e", e)
+
+    return (None, None)
+
+
+def get_random_sample(
+        file_path: str,
+        sample_size: int) -> pd.DataFrame | None:
+    '''Get a random sample of given size from a CSV'''
+    try:
+
